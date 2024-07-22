b17d7a7a964e9be0d375c3e0d6eeb8de3b8ef500
bhavanashivade <commit2022@gmail.com>
2024-07-22 18:54:20 +0530

    file updated

---
+++ pictures/script.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+let sections = document.querySelectorAll('sections');
+let navLinks = document.querySelectorAll('.header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+
+            })
+        }
+
+
+
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
+
+
+
+
+
+
+
+
+
+
